f557e8c14d04cade20c0756085c59552066c4199
Freeman0205 <61151465+Freeman0205@users.noreply.github.com>
2020-03-24 09:41:07 +0500

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,6 +1,6 @@
 def anagram(w1, w2):
-        w1 = w1.lower()
+        w1 = w1.lower()    # Исключаем влияние регистра
         w2 = w2.lower()
-        return  sorted(w1) == sorted(w2)
+        return  sorted(w1) == sorted(w2) # сортируем слова по алфавиту и сравниваем значения(Try если слова анаграммы)
 print(anagram("Капот", "тапок"))
 print(anagram("summer", "spring"))
